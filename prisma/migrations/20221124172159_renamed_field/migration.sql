82c63bb27c9d7ff0bd75521591f90046f9ab83a0
Luca Di Bello <info@lucadibello.ch>
2023-01-05 11:39:34 +0100

    Removed boilerplate migrations

--- prisma/migrations/20221124172159_renamed_field/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `emailToken` on the `User` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "User" DROP COLUMN "emailToken",
-ADD COLUMN     "twoFactorToken" TEXT;
