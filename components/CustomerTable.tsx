f7f1520295c813ae25bc0599c5b7de011de8e539
Luca Di Bello <info@lucadibello.ch>
2023-01-26 15:54:21 +0100

    Created custom CustomerTable

--- components/CustomerTable.tsx
+++ components/CustomerTable.tsx
@@ -1,10 +1,45 @@
 import { Box } from "@chakra-ui/react"
+import { SWRResponse } from "swr"
+import { CustomersApiResponse } from "../lib/types/apis/customers"
+import { createColumnHelper, DeepKeys } from '@tanstack/react-table'
+import { Customer } from "@prisma/client"
+import { DataTable } from "./DataTable"
 
+export type CustomerTableAvailableColumns = Array<keyof Customer>
+interface ICustomerTableProps {
+  customers: SWRResponse<CustomersApiResponse>
+  columns: CustomerTableAvailableColumns
+}
 
-const CustomerTable = () => {
+const columnHelper = createColumnHelper<Customer>();
+
+export const defaultColumns: DeepKeys<Customer>[] = [
+  'CustomerId',
+  'FirstName',
+  'LastName',
+  'Company',
+  'Country',
+  'City',
+  'Phone',
+  'Address',
+  'Email',
+]
+
+const generateColumns = (columns: DeepKeys<Customer>[]) => {
+  return columns.map(column => {
+    return columnHelper.accessor(column, {
+      cell: (row) => row.getValue(),
+    })
+  })
+}
+
+const CustomerTable = ({ customers, columns }: ICustomerTableProps) => {
   return (
     <Box>
-      <h1>Customer Table</h1>
+      <DataTable
+        data={customers.data?.data?.customers || []}
+        columns={generateColumns(columns)}
+      />
     </Box>
   )
 }
