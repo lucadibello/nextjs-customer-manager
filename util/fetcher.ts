9e2852066d2d05555a45137810a72501773e6410
Luca Di Bello <info@lucadibello.ch>
2023-01-27 14:06:21 +0100

    Updated fetcher

--- util/fetcher.ts
+++ util/fetcher.ts
@@ -22,62 +22,69 @@ const fetcher = <T extends ApiResponse<any>>(
       'Content-Type': 'application/json',
     },
     body: data ? JSON.stringify(data) : undefined,
-  }).then(async res => {
-    // Check if response is ok
-    if (res.status == 498) {
-      // If not, check if we are already retrying
-      if (isRetrying) {
-        onErrorNewAuthNeeded && onErrorNewAuthNeeded()
-        throw new Error('Unable to refresh token')
-      }
+  })
+    .then(async res => {
+      // Check if response is ok
+      if (res.status == 498) {
+        // If not, check if we are already retrying
+        if (isRetrying) {
+          onErrorNewAuthNeeded && onErrorNewAuthNeeded()
+          throw new Error('Unable to refresh token')
+        }
 
-      // Access refresh token via local storage
-      const refreshToken = localStorage.getItem('refreshToken')
+        // Access refresh token via local storage
+        const refreshToken = localStorage.getItem('refreshToken')
 
-      // Token expired, refresh it and retry the request using recursion
-      if (refreshToken) {
-        return fetch('/api/refresh', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({ refreshToken }),
-        })
-          .then(refreshRes => refreshRes.json() as Promise<RefreshApiResponse>)
-          .then(refreshRes => {
-            if (refreshRes.success && refreshRes.data) {
-              // Update the new access token in cookies
-              setCookie('token', refreshRes.data.token, {
-                sameSite: true,
-              })
+        // Token expired, refresh it and retry the request using recursion
+        if (refreshToken) {
+          return fetch('/api/refresh', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ refreshToken }),
+          })
+            .then(
+              refreshRes => refreshRes.json() as Promise<RefreshApiResponse>
+            )
+            .then(refreshRes => {
+              if (refreshRes.success && refreshRes.data) {
+                // Update the new access token in cookies
+                setCookie('token', refreshRes.data.token, {
+                  sameSite: true,
+                })
 
-              // Retry the request
-              return fetcher<T>(url, data, true)
-            } else {
+                // Retry the request
+                return fetcher<T>(url, data, true)
+              } else {
+                onErrorNewAuthNeeded && onErrorNewAuthNeeded()
+                throw new Error('Unable to refresh token')
+              }
+            })
+            .catch(() => {
+              // Redirect to login page if refresh token is invalid
               onErrorNewAuthNeeded && onErrorNewAuthNeeded()
               throw new Error('Unable to refresh token')
-            }
-          })
-          .catch(() => {
-            // Redirect to login page if refresh token is invalid
-            onErrorNewAuthNeeded && onErrorNewAuthNeeded()
-            throw new Error('Unable to refresh token')
-          })
-      } else {
-        throw new Error('Missing refresh token. Please login again.')
-      }
-    } else {
-      const responseJson: T = await res.json()
-      if (!responseJson.success) {
-        // Check if response code is 401 (unauthorized)
-        if (res.status == 401) {
-          onErrorNewAuthNeeded && onErrorNewAuthNeeded()
+            })
+        } else {
+          throw new Error('Missing refresh token. Please login again.')
         }
-        throw new Error(responseJson.message)
+      } else {
+        const responseJson: T = await res.json()
+        if (!responseJson.success) {
+          // Check if response code is 401 (unauthorized)
+          if (res.status == 401) {
+            onErrorNewAuthNeeded && onErrorNewAuthNeeded()
+          }
+          throw new Error(responseJson.message)
+        }
+        return responseJson
       }
-      return responseJson
-    }
-  })
+    })
+    .catch(err => {
+      onErrorNewAuthNeeded && onErrorNewAuthNeeded()
+      throw err
+    })
 }
 
 export const swrFetcher = <T extends ApiResponse<any>>(
